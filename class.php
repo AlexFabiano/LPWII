5a4c1cf46aba47b49132690246d6d9918d52047c
AlexFabiano <alex_fabino1993@hotmail.com>
2015-09-29 14:48:03 -0300

    Novo exercicio de lpw.

---
+++ class.php
@@ -0,0 +1,40 @@
+<?php
+
+include 'html.php';
+
+class form extends Html {
+    
+    public function getCriarFormulario() {
+        
+        $action = "";
+        $method.= ""; 
+    }
+    
+    public function getInput() {
+        
+        $label ="";
+        $name.="";
+        $type.="";
+        $value.="";
+    }    
+    
+    public function getBtnEnviar() {
+        
+        $value ="";
+    }
+    
+    public function getFinalizaFormulario(){
+        
+    } 
+        
+    
+    
+}
+
+class Heradar extends Html{
+    
+}
+
+
+
+?>
